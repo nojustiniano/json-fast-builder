769c1b741f21f022bfe2350d12e102ace361876d
Hernán <nojustiniano@gmail.com>
2016-11-25 00:04:58 -0300

    Initial commit

---
+++ src/main/java/org/hch/jsonfast/JsonArrayBuilder.java
@@ -0,0 +1,49 @@
+package org.hch.jsonfast;
+
+/**
+ * Created by hernan on 24/11/16.
+ */
+public class JsonArrayBuilder {
+    private static final char[] NULL = {'n','u','l','l'};
+    private static final char TS = '"';
+    private static final char FS = ',';
+    private StringBuilder stringBuilder;
+
+
+    public JsonArrayBuilder(){
+        stringBuilder = new StringBuilder(255).append('[');
+    }
+
+    public JsonArrayBuilder put(String value){
+        if(value == null)
+            return putNull();
+
+        stringBuilder
+                .append(TS)
+                .append(value)
+                .append(new char[]{TS,FS});
+        return this;
+    }
+
+    public JsonArrayBuilder put(Object value){
+        stringBuilder
+                .append(value)
+                .append(FS);
+        return this;
+    }
+
+    public JsonArrayBuilder putNull(){
+        stringBuilder
+                .append(NULL)
+                .append(FS);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        stringBuilder.setLength(stringBuilder.length()-1);
+        stringBuilder.append(']');
+
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/org/hch/jsonfast/JsonObjectBuilder.java
@@ -0,0 +1,58 @@
+package org.hch.jsonfast;
+
+/**
+ * Created by hernan on 24/11/16.
+ */
+public class JsonObjectBuilder {
+
+    private static final char[] NULL = {'n','u','l','l'};
+    private static final char TS = '"';
+    private static final char FS = ',';
+    private StringBuilder stringBuilder;
+
+
+    public JsonObjectBuilder(){
+        stringBuilder = new StringBuilder(255).append('{');
+    }
+
+    public JsonObjectBuilder put(String key, String value){
+        if(value == null)
+            return putNull(key);
+
+        stringBuilder
+                .append(TS)
+                .append(key)
+                .append(new char[]{TS, ':', TS})
+                .append(value)
+                .append(new char[]{TS,FS});
+        return this;
+    }
+
+    public JsonObjectBuilder put(String key, Object value){
+        stringBuilder
+                .append(TS)
+                .append(key)
+                .append(new char[]{TS, ':'})
+                .append(value)
+                .append(FS);
+        return this;
+    }
+
+    public JsonObjectBuilder putNull(String key){
+        stringBuilder
+                .append(TS)
+                .append(key)
+                .append(new char[]{TS, ':'})
+                .append(JsonObjectBuilder.NULL)
+                .append(FS);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        stringBuilder.setLength(stringBuilder.length()-1);
+        stringBuilder.append('}');
+
+        return stringBuilder.toString();
+    }
+}
